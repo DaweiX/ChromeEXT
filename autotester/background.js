ab8eb059756904f0af21b9f5b86e2a5bd3aefa44
DaweiX <DaweiX@outlook.com>
2021-01-11 11:05:06 +0800

    Delete background.js

--- autotester/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-	    var id = response.id;
-	    var msg = response.msg;
-	    var http = response.http;
-	    addlog('uuid: ' + id, 'black', 'FINGER')
-		chrome.management.setEnabled(id, false);
-		addlog('Extension ' + id + ' has been disabled', 'blue');
-		return true;
-});
